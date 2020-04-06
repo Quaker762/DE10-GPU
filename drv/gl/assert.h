a79ab970c6184e05c1fb2a4604f1d781bcacd665
Jesse Buhagiar <jesse.buhagiar@student.rmit.edu.au>
2020-04-06 21:19:18 +1000

    Meta: Update license and driver header files

--- drv/gl/assert.h
+++ drv/gl/assert.h
@@ -1,3 +1,15 @@
+/**
+ * Debug Assertion macros to aid in debugging.
+ *
+ *
+ * Copyright (c) 2020, Jesse Buhagiar, Jack Allan and Matthew Atkin
+ * All rights reserved.
+ * Unauthorized copying or reproduction of this project and the files contained within
+ * is strictly prohibited.
+ *
+ * Author: Jesse Buhagiar
+ * Date: 4/4/2020
+ */
 #pragma once
 
 #include <cassert>
