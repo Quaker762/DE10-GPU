f713fc058ca32c73961dc56e6e281f72c03475a3
Jesse Buhagiar <jesse.buhagiar@student.rmit.edu.au>
2020-04-08 00:38:34 +1000

    GL: Implement `glGetString()`
    Fun vanity function that quake uses to display some information
    about our driver (the vendor, version number etc).

--- drv/gl/glutil.cpp
+++ drv/gl/glutil.cpp
@@ -10,8 +10,14 @@
 #include "assert.h"
 #include "gl.h"
 #include "glcontext.h"
+#include "log.hpp"
 
 #include <cmath>
+#include <string>
+
+static const std::string s_vendor = "SOCFX Interactive"; // Ha ha ha. Change this when we have a name lmfao
+static const std::string s_renderer = "Rush3D Cyclone V Graphics Accelerator";
+static const std::string s_version = "OpenGL 1.2 SOCFX Interactive";
 
 void glClear(GLbitfield mask)
 {
@@ -38,4 +44,25 @@ void glClearColor(GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha)
     g_gl_state.clear_color.g = green;
     g_gl_state.clear_color.b = blue;
     g_gl_state.clear_color.a = alpha;
+}
+
+GLubyte* glGetString(GLenum name)
+{
+    switch(name)
+    {
+    case GL_VENDOR:
+        return reinterpret_cast<GLubyte*>(const_cast<char*>(s_vendor.c_str()));
+    case GL_RENDERER:
+        return reinterpret_cast<GLubyte*>(const_cast<char*>(s_renderer.c_str()));
+    case GL_VERSION:
+        return reinterpret_cast<GLubyte*>(const_cast<char*>(s_version.c_str()));
+    default:
+#ifdef DEBUG
+        log(LogLevel::WARN, "glGetString(): Unknown enum name!\n");
+#endif
+        break;
+    }
+
+    // TODO: Set glError to GL_INVALID_ENUM here
+    return 0;
 }
