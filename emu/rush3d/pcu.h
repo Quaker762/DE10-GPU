8cb2edd30c3a6b11a31ba7cf8c979d1114a86a16
Jesse Buhagiar <jesse.buhagiar@student.rmit.edu.au>
2020-04-07 22:57:40 +1000

    Emulator: Major emulator overhaul
    The chip simulator now gets compiled as a static library. This means
    we can detach the simulator and actually issue it commands via
    the OpenGL driver, instead of explicitly having a program that runs
    the simulator. This way we can compile and run any OpenGL program
    that we like.

--- emu/rush3d/pcu.h
+++ emu/rush3d/pcu.h
@@ -1,5 +1,11 @@
 /**
- * Core GPU
+ * Copyright (c) 2020, Jesse Buhagiar, Jack Allan and Matthew Atkin
+ * All rights reserved.
+ * Unauthorized copying or reproduction of this project and the files contained within
+ * is strictly prohibited.
+ *
+ * Author: Jesse Buhagiar
+ * Date: 4/4/2020
  */
 #pragma once
 
@@ -10,8 +16,8 @@
 #include <queue>
 
 #define FRAMEBUFFER_MEMORY_SIZE 2 * MB
-#define DEFAULT_WIDTH 800
-#define DEFAULT_HEIGHT 600
+#define DEFAULT_WIDTH           800
+#define DEFAULT_HEIGHT          600
 
 struct FrameBufferWindow
 {
@@ -27,30 +33,27 @@ enum Commands
 enum RegisterOffsets
 {
     STATUS = 0,
-    DBGR,
-    DBGG,
-    DBGB
+    fbCOLOR,
+    fbFILL
 };
 
 namespace Chip
 {
-
 /**
  * Framebuffer interface (AKA Pixel Control Unit [PCU])
- * 
+ *
  * This is effectively the "core" graphics chip. It's job is to manage
  * blitting everything to the framebuffer. It also contains the register
- * file for the entire GPU as well as 
- * 
+ * file for the entire GPU as well as
+ *
  * We have 2MiB of framebuffer, which supports a resolution up to 800x600x24.
  * At the moment, we only support 24 bit color (no alpha channel)
- * 
+ *
  * Very similar to the SST-1. Verbal information on how this system worked can be
  * found here: https://youtu.be/3MghYhf-GhU?t=3264
  */
 class PCU
 {
-
 public:
     PCU() = delete;
     PCU(const SDL_Surface*);
@@ -59,7 +62,6 @@ public:
     void set_window_size(int, int);
 
     void push_fifo(uint32_t);
-    void handle_command(uint32_t);
     void write_register(RegisterOffsets offset, uint32_t value);
     uint32_t read_register(RegisterOffsets offset);
 
@@ -67,12 +69,13 @@ public:
 
 private:
     void put_pixel(int, int, uint32_t pixel);
+    void fill_framebuffer();
 
 private:
     uint32_t* m_framebuffer;
     uint32_t* m_backbuffer;
 
-    FrameBufferWindow m_window_size { 800, 600 };
+    FrameBufferWindow m_window_size{ 800, 600 };
 
     std::queue<uint32_t> m_fifo;
 
