23c8302bcb9141cf240c138587151f9655821004
Jesse Buhagiar <jesse.buhagiar@student.rmit.edu.au>
2020-04-12 23:35:50 +1000

    GL: Add copyright header to mat4.h

--- drv/gl/math/mat4.h
+++ drv/gl/math/mat4.h
@@ -1,5 +1,11 @@
 /**
+ * Copyright (c) 2020, Jesse Buhagiar, Jack Allan and Matthew Atkin
+ * All rights reserved.
+ * Unauthorized copying or reproduction of this project and the files contained within
+ * is strictly prohibited.
  *
+ * Author: Jesse Buhagiar
+ * Date: 2/4/2020
  */
 #pragma once
 
