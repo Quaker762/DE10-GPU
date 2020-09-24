450eb9950f0f2b1019165c38916a2a329de87957
Jesse Buhagiar <jesse.buhagiar@student.rmit.edu.au>
2020-09-25 00:08:39 +1000

    R3D: Fix map range
    Map up to 0x60. Should probably test for this somewhere in
    the driver so we don't get any nasty UB

--- drv/r3d/altera.cpp
+++ drv/r3d/altera.cpp
@@ -18,7 +18,7 @@
 #include <sys/mman.h>
 
 static constexpr uint32_t RUSH3D_ADDRESS = 0xc0000000;
-static constexpr uint8_t RUSH3D_ADDRESS_RANGE = 0x3f;
+static constexpr uint8_t RUSH3D_ADDRESS_RANGE = 0x60;
 
 static AlteraBridge* s_the = nullptr;
 
