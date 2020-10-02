c84d7aa87cc8b5345a1e6cdb4024c6bd5e94cf19
Jesse Buhagiar <jesse.buhagiar@student.rmit.edu.au>
2020-10-03 01:27:00 +1000

    Tests: Push up the fucking test so it doesn't get overwritten!

--- qnx/tests/GLTest2/src/GLTest2.cpp
+++ qnx/tests/GLTest2/src/GLTest2.cpp
@@ -1,17 +1,34 @@
-/**
- * test00.cpp
- *
- * Clears the screen colour to red
- */
-#include <gl.h>
+#include <GL/gl.h>
 #include <r3d/rush3d.h>
 #include <iostream>
 #include <sys/neutrino.h>
 #include <cstdlib>
 #include <cmath>
 
-#define VIDEO_WIDTH 640
-#define VIDEO_HEIGHT 480
+#define VIDEO_WIDTH  800
+#define VIDEO_HEIGHT 600
+
+#define NUM_CUBE_VERTICES 108
+
+static const GLfloat g_vertex_buffer_data[] = { -1.0f, -1.0f, -1.0f,                      // triangle 1 : begin
+                                                -1.0f, -1.0f, 1.0f,  -1.0f, 1.0f,  1.0f,  // triangle 1 : end
+                                                1.0f,  1.0f,  -1.0f,                      // triangle 2 : begin
+                                                -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,  -1.0f, // triangle 2 : end
+                                                1.0f,  -1.0f, 1.0f,  -1.0f, -1.0f, -1.0f, 1.0f,  -1.0f, -1.0f, 1.0f,  1.0f,  -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f,  -1.0f, 1.0f,  -1.0f, 1.0f, -1.0f, 1.0f,
+                                                -1.0f, -1.0f, 1.0f,  -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,  1.0f,  -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  -1.0f, -1.0f, 1.0f, 1.0f,  1.0f,
+                                                1.0f,  -1.0f, 1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  -1.0f, -1.0f, 1.0f,  -1.0f, 1.0f, 1.0f,  1.0f,  -1.0f, 1.0f,  -1.0f, -1.0f, 1.0f,  1.0f,  1.0f,  1.0f, 1.0f,  -1.0f, 1.0f,  1.0f,  1.0f, -1.0f, 1.0f };
+
+void draw_cube()
+{
+    for(int i = 0; i < NUM_CUBE_VERTICES; i += 9)
+    {
+        glBegin(GL_TRIANGLES);
+        glVertex3f(g_vertex_buffer_data[i], g_vertex_buffer_data[i + 1], g_vertex_buffer_data[i + 2]);
+        glVertex3f(g_vertex_buffer_data[i + 3], g_vertex_buffer_data[i + 4], g_vertex_buffer_data[i + 5]);
+        glVertex3f(g_vertex_buffer_data[i + 6], g_vertex_buffer_data[i + 7], g_vertex_buffer_data[i + 8]);
+        glEnd();
+    }
+}
 
 int main(int, char**)
 {
@@ -34,21 +51,37 @@ int main(int, char**)
     xmin = ymin * aspect;
     xmax = ymax * aspect;
     glFrustum(xmin, xmax, ymin, ymax, 4, 10);
-
+    GLfloat angle = 0.0f;
+    GLfloat z = -8.0f;
     while(running)
     {
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        angle -= 0.9f;
+
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-        glBegin(GL_TRIANGLES);
-        	glColor3f(0.0f, 1.0f, 0.0f);
-        	glVertex3f(-1.0f, -1.0f, -2.1f); // Top left corner (the origin)
-        	glVertex3f(-1.0f, 1.0f, -2.1f); // Bottom left corner
-        	glVertex3f(1.0f, 1.0f, -2.1f); // Bottom right corner
-        glEnd();
+        glTranslatef(0.0f, 0.0f, 0.0f);
+        glRotatef(angle, 0.0f, 1.0f, 1.0f);
+        glTranslatef(0.0f, 0.0f, z);
+        draw_cube();
 
+        /** Uncomment me for more cubes!
+        glLoadIdentity();
+        glTranslatef(0.0f, 0.0f, 0.0f);
+        glRotatef(angle, 0.0f, 1.0f, 0.0f);
+        glTranslatef(-4.0f, 0.0f, -15.0f);
+        draw_cube();
+
+        glLoadIdentity();
+        glTranslatef(0.0f, 0.0f, 0.0f);
+        glRotatef(angle, 1.0f, 1.0f, 0.0f);
+        glTranslatef(4.0f, 3.0f, -12.0f);
+        draw_cube();
+        **/
+
+        rush3d_swap_buffers();
     }
 
     return 0;
